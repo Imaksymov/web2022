82e22955c4f1005047b858a9bdc418562d92cb02
Іван Максимов <wano20022@gmail.com>
2022-02-15 16:24:24 +0200

    first commit

---
+++ lab1/src/main/java/tools/Tools.java
@@ -0,0 +1,44 @@
+package tools;
+
+public class Tools {
+	private String title;
+	private int weight;
+	private float width;
+	
+	public Tools(String title, int weight, float width) {
+		super();
+		this.title = title;
+		this.weight = weight;
+		this.width = width;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setAge(int weight) {
+		this.weight = weight;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	@Override
+	public String toString() {
+		return "{\"title\": \""+title+"\", \"weight\": "+weight+", \"width\":"+width+"}";
+	}
+	
+}
